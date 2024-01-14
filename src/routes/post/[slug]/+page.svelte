c05393f8db48ad207bc7f2f0e93f702e641741d5
Aaron Mamparo <aaronmamparo@gmail.com>
2024-01-13 22:11:00 -0600

    fix check

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import BlogPost from '$lib/components/BlogPost/BlogPost.svelte'
+	import type { BlogPostMetadata } from '$lib/blog'
 
-	export let data: BlogPost
+	export let data: {
+		metadata: BlogPostMetadata
+		content: string
+	}
 </script>
 
 <BlogPost blogPost={data} />
