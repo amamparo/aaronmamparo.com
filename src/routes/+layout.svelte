9b8d2783b3f700ff216c48dc25747b27cbb9fcf4
Aaron Mamparo <aaronmamparo@gmail.com>
2024-01-14 18:26:21 -0600

    css fuck

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
 	import Link from './Link.svelte'
 </script>
 
-<div class="leading-6 px-4 my-12 mx-auto flex flex-col md:flex-row justify-center">
-	<div class="p-4 md:w-36">
+<div class="leading-6 px-4 my-12 mx-auto flex flex-col justify-center md:flex-row md:max-w-4xl">
+	<div class="p-4 md:min-w-36">
 		<nav
 			class="pb-8 border-b border-gray-200 flex flex-row md:border-b-0 md:border-r md:flex-col md:pb-0"
 		>
@@ -13,7 +13,7 @@
 			<Link href="/atom.xml">RSS</Link>
 		</nav>
 	</div>
-	<div class="p-4 flex-grow md:max-w-3xl">
+	<div class="p-4 flex-grow">
 		<slot />
 	</div>
 </div>
